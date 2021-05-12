a9634c15cb8e42edf7115fd81b2db8b5ef0ea5a9
Demian Holmberg <dholmberg@naturalinsight.com>
2021-05-12 17:19:26 -0400

    first commit

---
+++ shared/components/WTF/WTF.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+/* built CORE tough */
+
+export class WTF extends React.Component {
+  render() {
+    return (
+      <p>WTF</p>
+    );
+  }
+}
+
+export default WTF;
---
+++ shared/components/WTF3/WTF3.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class WTF3 extends React.Component {
+  render() {
+    return React.createElement(
+      'div',
+      {className: 'sidebar'},
+      'WTF 3, im back and pissed!'
+    );
+  }
+}
+
+export default WTF3;
